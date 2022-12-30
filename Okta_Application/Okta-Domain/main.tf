7679d9a03a3c3adca9b946c01afbf609b9a524d6
Vishal Joshi <vijoshi@bebotechnologies.com>
2022-12-30 15:45:01 +0530

    script updated, Okta_Domain

---
+++ Okta_Application/Okta-Domain/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    okta = {
+      source = "okta/okta"
+      version = "~> 3.39.0"
+    }
+  }
+}
+# Configure the Okta Provider - API token set in env var.
+provider "okta" {
+ org_name  = var.org_name
+ base_url  = var.base_url
+ api_token = var.api_token
+}
+
+
