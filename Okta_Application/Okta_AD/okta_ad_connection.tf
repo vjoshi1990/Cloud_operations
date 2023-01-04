2d1c169682ad3a7fa30703afe6e90d7a6f3797ea
Vishal Joshi <vijoshi@bebotechnologies.com>
2023-01-04 19:16:40 +0530

    updating AD file

--- Okta_Application/Okta_AD/okta_ad_connection.tf
+++ Okta_Application/Okta_AD/okta_ad_connection.tf
@@ -6,3 +6,8 @@ resource "oktapam_ad_connection" "Window_AD" {
     service_account_password = "xxxxxxxxxxxx"
 }
 
+resource "oktapam_ad_task_settings" "Setting_WINDOW_AD" {
+    connection_id = "4464462dsdsdasdsisiwwd"
+    is_active = "true"
+      
+}
