1cd1f8bffc7a5d83936c83af9d4e4bdaf5d43c15
Vishal Joshi <vijoshi@bebotechnologies.com>
2023-01-10 17:36:30 +0530

    terraform update

--- Okta_testing/main.tf
+++ Okta_testing/main.tf
@@ -1,10 +1,15 @@
-resource "okta_user_type" "test" {
-  name   = "example"
-  display_name = "example"
-  description = "example"
+#Creating a Group
+resource "okta_group" "testgroup2" {
+  name        = "MFAExampt2"
+  description = "My Example Group"
+  skip_users  = true 
 }
 
-resource "okta_group" "test" {
-  name        = "test"
-  description = "My test Group"
+  resource "okta_policy_password" "example" {
+  name                   = "example"
+  status                 = "ACTIVE"
+  description            = "Example"
+  password_history_count = 4
+  question_recovery = "INACTIVE"
+  groups_included = ["${okta_group.testgroup2.id}"]
 }
